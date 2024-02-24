7ac2a252ddfaab39b259b9dbe7f2d9591c002756
Hussein Akar <hak@container-xchange.com>
2024-02-24 15:46:53 +0200

    initial commit

---
+++ src/main/java/com/project/numberconverterbackend/NumberConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.numberconverterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberConverterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberConverterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/numberconverterbackend/NumberConverterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.numberconverterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberConverterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
