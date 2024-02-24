a7ffafa5342ccb80b48200398ee983260b220d7e
Hussein Akar <hak@container-xchange.com>
2024-02-24 18:37:46 +0200

    fix: allow cross-origin to allow angular app to connect

--- src/main/java/com/project/numberconverterbackend/controller/NumberConverterController.java
+++ src/main/java/com/project/numberconverterbackend/controller/NumberConverterController.java
@@ -5,11 +5,13 @@ import com.project.numberconverterbackend.service.NumberConverterService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/number-converters")
